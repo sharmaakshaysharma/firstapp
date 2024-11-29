71f252eca562033c460c7b388ae7bcdc44fb46d7
sharmaakshaysharma <akshaysharma38256@gmail.com>
2024-11-30 00:26:19 +0530

    akshay_30/10/2024

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from cart.views import *
+
+urlpatterns =[
+    path('addtocart/<int:product_id>/',cart_page,name='cart_page'),
+    path('showcart/',show_cart,name='show_cart'),
+    
+]
+
--- store/views.py
+++ store/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .forms import *
 from django.http import HttpResponse,JsonResponse
 from django.contrib import messages
+from cart.models import Cart
 
 # Create your views here.
-
-def home_page(request):
-    return render(request,'store/home.html')  
-
 
 def category_page(request):
     if request.method == 'POST':
@@ -19,11 +16,8 @@ def category_page(request):
             return redirect('category_page')
     else:
         form = CategoryType()
-
     categories = Category.objects.all()     
     return render(request, 'store/category.html', {'form': form, 'categories': categories})
-
-
 def update_category(request):
     if request.method == 'POST':
         category_id = request.POST.get('category_id')
@@ -32,15 +26,12 @@ def update_category(request):
         if form.is_valid():
             form.save()
     return redirect('category_page')
-
 def delete_category(request):
     if request.method == 'POST':
         category_id = request.POST.get('category_id')
         category = get_object_or_404(Category, id=category_id)
         category.delete()
     return redirect('category_page')
-
-
 def coverType_page(request):
     if request.method == 'POST':
         form=CoverTypes(request.POST)
@@ -49,11 +40,8 @@ def coverType_page(request):
             return redirect('coverType_page')
     else:
         form=CoverType()
-
     covertypes=CoverType.objects.all()
     return render(request,'store/coverType.html',{'form':form,'covertypes':covertypes})
-
-
 def update_coverType(request):
     if request.method == 'POST':
         coverType_id = request.POST.get('coverType_id')
@@ -62,7 +50,6 @@ def update_coverType(request):
         if form.is_valid():
             form.save()
     return redirect('coverType_page')
-
 def delete_coverType(request):
     if request.method == 'POST':
         coverType_id = request.POST.get('coverType_id')
@@ -71,9 +58,12 @@ def delete_coverType(request):
     return redirect('coverType_page')
 
 def get_product(request):
-    product=Product.objects.all()
-    return render(request,'store/home.html',{'product':product})
-
+    if request.user.is_authenticated:
+        cart_item_count = Cart.objects.filter(user=request.user).count()
+    else:
+        cart_item_count = 0
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'cart_item_count': cart_item_count, 'product': products})
 
 def product_page(request):
     if request.method == 'POST':
@@ -85,11 +75,8 @@ def product_page(request):
             print("Form errors:", form.errors)  
     else:
         form = ProductForm()
-
     products = Product.objects.all()
     return render(request, 'store/product.html', {'form': form, 'products': products})
-
-
 def update_product(request):
     if request.method == 'POST':
         product_id = request.POST.get('product_id')
@@ -105,15 +92,3 @@ def delete_product(request):
         product=get_object_or_404(Product,id=product_id)
         product.delete()
     return redirect('product_page')
-
-
-# def view_product(request, product_id):
-#     product = get_object_or_404(Product, id=product_id)
-#     product_data = {
-#         'title': product.title,
-#         'author': product.author,
-#         'description': product.description,
-#         'price': product.price,
-#         'image_url': product.image.url,
-#     }
-#     return JsonResponse(product_data)

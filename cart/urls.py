d7f441e3a6c598a6809d049ad644a674b1d879f8
Akshay <156594334+sharmaakshaysharma@users.noreply.github.com>
2024-12-02 15:54:47 +0530

    cart changes (#7)
    Co-authored-by: akshay <akshaykumarsharma@cssoftsolutions.com>

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from cart.views import *
 urlpatterns =[
     path('addtocart/<int:product_id>/',cart_page,name='cart_page'),
     path('showcart/',show_cart,name='show_cart'),
-    
+    path('update-cart/', update_cart, name='update_cart'),
 ]
 
--- firstapp/urls.py
+++ firstapp/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('accounts/', include('accounts.urls')),
-    path('cart/',include('cart.urls'))
+    path('cart/',include('cart.urls')),
 ]
 
 if settings.DEBUG:
